5f8c5af2ba553d29392500efda64f7531e9229c0
Ramona Rajagopalan <65932912+flinksey@users.noreply.github.com>
2024-12-17 01:03:42 +0800

    Delete Datasets/Extraction_2023.r
    Deleting as I've uploaded the file I actually used

--- Datasets/Extraction_2023.r
+++
@@ -1,5 +0,0 @@
-# will add the libraries used once I confirm -- code used is still messy
-
-# the data from the CDC BRFSS database is not readily available in CSV format so I downloaded the SAS ZIP file and then converted it into CSV
-LLCP2023_data <- read_xpt("LLCP2023.XPT")
-write.csv(LLCP2023_data,"2023-brfss-subset-xpt.csv")
